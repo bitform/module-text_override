dce96481e42c75e6bcd8651d9338865a0abcaed3
Ben Keen <ben.keen@gmail.com>
2014-05-17 16:05:59 -0700

    1.0.0

--- global/scripts/text_override.js
+++ global/scripts/text_override.js
@@ -2,46 +2,45 @@ var to_ns = {};
 to_ns.num_rows = null; // set by page
 to_ns.deleted_rows = [];
 
-$(function() {
-  $("form").bind("submit", function() {
-    $(".string_value").removeAttr("readonly");
-  });
-});
 
-
-to_ns.display_string = function(placeholder, row) {
-  $("#string_" + row).val($("#string__" + placeholder).html())
+to_ns.display_string = function(placeholder, row)
+{
+  $("string_" + row).value = $("string__" + placeholder).innerHTML;  
 }
 
 
 /**
  * Deletes a row.
  */
-to_ns.delete_row = function(row) {
-  $("#row_" + row).hide("blind");
+to_ns.delete_row = function(row)
+{
+  Effect.Fade($("row_" + row), { duration: 0.8 });
+  Effect.BlindUp($("row_" + row), { duration: 0.8 });
+
   to_ns.deleted_rows.push(row);
-  $("#deleted_rows").val(to_ns.deleted_rows.join(","));
+  $("deleted_rows").value = to_ns.deleted_rows.join(",");
 }
 
 
 /**
  * Adds a new blank row.
  */
-to_ns.add_row = function() {
+to_ns.add_row = function()
+{
   var curr_row = ++to_ns.num_rows;
 
   // get the template row
-  var row_html = $("#row_template").html();
+  var row_html = $("row_template").innerHTML;
   row_html = row_html.replace(/%%ROW%%/g, curr_row);
 
   // wrap the new line in a DIV, to allow easy removal if needed
   var wrapper_div = document.createElement("div");
   wrapper_div.setAttribute("id", "row_" + curr_row);
   wrapper_div.innerHTML = row_html;
-
+  
   // now append the new row to the main rows element
-  $("#rows").append(wrapper_div);
-
+  $("rows").insert(wrapper_div);
+  
   // store the total number of rows
-  $("#num_rows").val(curr_row);
+  $("num_rows").value = curr_row;
 }
--- module.php
+++ module.php
@@ -7,13 +7,14 @@
 $MODULE["author"]          = "Encore Web Studios";
 $MODULE["author_email"]    = "formtools@encorewebstudios.com";
 $MODULE["author_link"]     = "http://www.encorewebstudios.com";
-$MODULE["version"]         = "1.1.3";
-$MODULE["date"]            = "2011-11-22";
+$MODULE["version"]         = "1.0.0";
+$MODULE["date"]            = "2009-04-10";
 $MODULE["origin_language"] = "en_us";
+$MODULE["supports_ft_versions"] = "2.0.0";
 
 // define the module navigation - the keys are keys defined in the language file. This lets
 // the navigation - like everything else - be customized to the users language
 $MODULE["nav"] = array(
   "module_name" => array("index.php", false),
-  "word_help"   => array("help.php", true)
+  "word_help" => array("help.php", false)
     );
